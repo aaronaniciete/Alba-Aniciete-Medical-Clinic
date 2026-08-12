6a1f7beec192dd75f4a6a6d4ca0a06c392e73e98
aaronaniciete <aaronaniciete@gmail.com>
2026-08-12 10:06:17 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -268,7 +268,7 @@ function BookingForm({ onDone, onCancel }) {
 
   return (
     <div style={styles.card}>
-      <h1 style={styles.h1}>Patient & appointment details</h1>
+      <h1 style={styles.h1}>Appointment</h1>
 
       <SectionLabel>Patient information</SectionLabel>
       <Field label="Full name">
